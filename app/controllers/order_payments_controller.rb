861a66405687b9a9f52f26fec0c615fd2931d5ac
89Tain <manda.titani@gmail.com>
2025-02-04 09:38:28 +0200

    fix invalid auth csrm

--- app/controllers/order_payments_controller.rb
+++ app/controllers/order_payments_controller.rb
@@ -1,5 +1,6 @@
 
   class OrderPaymentsController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:void]
     def show
       # Implement the show action if needed
     end
