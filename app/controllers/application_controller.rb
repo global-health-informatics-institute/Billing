af2b461f148f102a58cea0057b8dc8eec4693409
ghii <nyaraihk@gmail.com>
2026-05-20 10:49:13 +0200

    removed home page btn on 600 err

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -99,7 +99,7 @@ class ApplicationController < ActionController::Base
            <p>Please contact support</p>
            <p>Something went wrong. We are working on it.</p>
            <div class="error-message">#{error_message}</div>
-           <%= link_to 'Back to Home', root_path, class: 'btn btn-primary', style: 'margin-top: 20px;' %>
+           <%= link_to ' to Home', root_path, class: 'btn btn-primary', style: 'margin-top: 20px;' %>
        </body>
        </html>
      HTML
@@ -155,7 +155,6 @@ class ApplicationController < ActionController::Base
            <p>Please contact support</p>
            <p>Something went wrong. We are working on it.</p>
            <div class="error-message">#{error_message}</div>
-           <%= link_to 'Back to Home', root_path, class: 'btn btn-primary', style: 'margin-top: 20px;' %>
        </body>
        </html>
      HTML
