e0848b05ecb6c17f18318f552c3484fa9130a7fb
ghii <nyaraihk@gmail.com>
2026-08-04 15:19:08 +0200

    add dental service id

--- app/models/service_type.rb
+++ app/models/service_type.rb
@@ -7,15 +7,15 @@ class ServiceType < ActiveRecord::Base
   def top_ten_services
     self.services.select(:name).limit(10).collect{|x| x.name}
   end
-  def child
-    self.services.select(:name).where(service_id: [3,7,8]).collect{|x| x.name}
-  end
-  def male
-    self.services.select(:name).where(service_id: [1,6,7,8]).collect{|x| x.name}
-  end
-  def female
-    self.services.select(:name).where(service_id: [2,4,5,6,7,8]).collect{|x| x.name}
-  end
+def child
+     self.services.select(:name).where(service_id: [3,7,8,9,10]).collect{|x| x.name}
+   end
+   def male
+     self.services.select(:name).where(service_id: [1,6,7,8,9,10]).collect{|x| x.name}
+   end
+   def female
+     self.services.select(:name).where(service_id: [2,4,5,6,7,8,9,10]).collect{|x| x.name}
+   end
   def number_of_services
     self.services.select("count(service_id) as count").first.count
   end
