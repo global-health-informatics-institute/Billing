10eda08b6f08f498b3b8c4a3b1ed6781e8e4f136
ghii <nyaraihk@gmail.com>
2026-08-19 16:25:09 +0200

    add service set migration

---
+++ db/migrate/20260819080000_create_service_sets.rb
@@ -0,0 +1,13 @@
+class CreateServiceSets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :service_sets, :primary_key => :service_set_id do |t|
+      t.string :name, null: false
+      t.integer :creator, null: false
+      t.boolean :voided, default: false
+      t.integer :voided_by
+      t.string :voided_reason
+      t.date :voided_date
+      t.timestamps null: false
+    end
+  end
+end
