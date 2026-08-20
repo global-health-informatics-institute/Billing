ffd27ff59eb4eab927082182395868b2993330d9
ghii <nyaraihk@gmail.com>
2026-08-20 09:18:07 +0200

    Update models for service_set associations

--- app/models/service_type.rb
+++ app/models/service_type.rb
@@ -5,17 +5,31 @@ class ServiceType < ActiveRecord::Base
   has_many :service_panels
 
   def top_ten_services
-    self.services.select(:name).limit(10).collect{|x| x.name}
+    services.order(:name).limit(10).pluck(:name)
   end
-def child
-     self.services.select(:name).where(service_id: [3,7,8,9,10]).collect{|x| x.name}
-   end
-   def male
-     self.services.select(:name).where(service_id: [1,6,7,8,9,10]).collect{|x| x.name}
-   end
-   def female
-     self.services.select(:name).where(service_id: [2,4,5,6,7,8,9,10]).collect{|x| x.name}
-   end
+
+  def all_service_names
+    services.order(:name).pluck(:name)
+  end
+
+  def child
+    set = ServiceSet.find_by(name: 'under_5')
+    return [] unless set
+    set.services.where(service_type_id: service_type_id).order(:name).pluck(:name)
+  end
+
+  def male
+    set = ServiceSet.find_by(name: 'male')
+    return [] unless set
+    set.services.where(service_type_id: service_type_id).order(:name).pluck(:name)
+  end
+
+  def female
+    set = ServiceSet.find_by(name: 'female')
+    return [] unless set
+    set.services.where(service_type_id: service_type_id).order(:name).pluck(:name)
+  end
+
   def number_of_services
     self.services.select("count(service_id) as count").first.count
   end
